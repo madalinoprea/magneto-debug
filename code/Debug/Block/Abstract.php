fbe935dfc86097aef79aa26c70931dcae525fc6c
Mario O <madalinoprea@gmail.com>
2016-02-03 14:59:51 +0200

    Adds requirest info (for future) and module's helper as protected member to our base block class

--- code/Debug/Block/Abstract.php
+++ code/Debug/Block/Abstract.php
@@ -1,6 +1,22 @@
 <?php
 class Sheep_Debug_Block_Abstract extends Mage_Core_Block_Template
 {
+    /** @var Sheep_Debug_Helper_Data */
+    protected $helper;
+
+
+    /**
+     * Sheep_Debug_Block_Abstract constructor.
+     *
+     * @param array $args
+     */
+    public function __construct(array $args)
+    {
+        parent::__construct($args);
+        $this->helper = Mage::helper('sheep_debug');
+    }
+
+
     public function getDefaultStoreId(){
         return Mage::app()
             ->getWebsite()
@@ -15,5 +31,14 @@ class Sheep_Debug_Block_Abstract extends Mage_Core_Block_Template
     public function getShowTemplateHints()
     {
         return false;
+    }
+
+    /**
+     * TODO: use a request info model
+     * @return Sheep_Debug_Model_Observer
+     */
+    public function getRequestInfo()
+    {
+        return Mage::getSingleton('sheep_debug/observer');
     }
 }
