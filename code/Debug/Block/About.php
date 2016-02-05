a85411ed99099594061bcccf444dd0fe33c28b5b
Mario O <madalinoprea@gmail.com>
2016-02-06 01:41:03 +0200

    Adds about and utils panels #42

--- code/Debug/Block/About.php
+++ code/Debug/Block/About.php
@@ -32,4 +32,35 @@ class Sheep_Debug_Block_About extends Sheep_Debug_Block_Panel
         return false;
     }
 
+    public function getMagentoVersion()
+    {
+        return Mage::getVersion();
+    }
+
+
+    public function isDeveloperMode()
+    {
+        return Mage::getIsDeveloperMode();
+    }
+
+
+    public function getExtensionStatus()
+    {
+        $status = array();
+
+        $extensions = $this->helper->getExtensionRequirements();
+        foreach ($extensions as $extension) {
+            $status [$extension] = extension_loaded($extension);
+        }
+
+        return $status;
+    }
+
+
+    public function getCurrentStore()
+    {
+        $currentStore = $this->_getApp()->getStore();
+        return sprintf("%s / %s", $currentStore->getWebsite()->getName(),  $currentStore->getName());
+
+    }
 }
