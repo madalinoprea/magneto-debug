1c438c4b1d3429c58f74677ef17f36358222c898
Mario O <madalinoprea@gmail.com>
2016-02-05 17:17:31 +0200

    Oops forgot to mark id as entity to get AUTOINCREMENT #41

--- code/Debug/sql/sheep_debug_setup/install-1.3.0.php
+++ code/Debug/sql/sheep_debug_setup/install-1.3.0.php
@@ -2,7 +2,7 @@
 /**
  * @codeCoverageIgnore
  */
- 
+
 /* @var $installer Mage_Core_Model_Resource_Setup */
 $installer = $this;
 
@@ -11,28 +11,29 @@ $installer->startSetup();
 $table = $installer->getConnection()
     ->newTable($installer->getTable('sheep_debug/request_info'))
     ->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
-        'nullable'  => false,
-        'primary' => 'true',
+        'identity' => true,
+        'nullable' => false,
+        'primary'  => true,
     ), 'Id')
     ->addColumn('token', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array(
-        'nullable'  => false,
+        'nullable' => false,
     ), 'Request Token')
     ->addColumn('http_method', Varien_Db_Ddl_Table::TYPE_VARCHAR, 32, array(
         'nullable' => false,
     ), 'Http Method')
     ->addColumn('request_path', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array(
-        'nullable'  => false,
+        'nullable' => false,
     ), 'Request Path')
     ->addColumn('response_code', Varien_Db_Ddl_Table::TYPE_TINYINT, null, array(
-        'nullable'  => false,
+        'nullable' => false,
     ), 'Http Response Status Code')
     ->addColumn('session_id', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array(
-        'nullable'  => true,
+        'nullable' => true,
     ), 'Session Id')
     ->addColumn('date', Varien_Db_Ddl_Table::TYPE_DATETIME, null, array(
-        'nullable'  => false,
+        'nullable' => false,
     ), 'Date')
-    ->addColumn('time', Varien_Db_Ddl_Table::TYPE_DECIMAL, array(15,2), array(
+    ->addColumn('time', Varien_Db_Ddl_Table::TYPE_DECIMAL, array(15, 2), array(
         'nullable' => true,
     ), 'Total Time')
     ->addColumn('peak_memory', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
@@ -40,12 +41,11 @@ $table = $installer->getConnection()
     ), 'Peak Memory used in bytes')
     ->addColumn('info', Varien_Db_Ddl_Table::TYPE_BLOB, 16777216, array(
         'nullable' => false,
-        'default' => '',
+        'default'  => '',
     ), 'Serialized Info')
     ->addIndex($this->getIdxName('sheep_debug/request_info', array('token')), array('token'))
     ->addIndex($this->getIdxName('sheep_debug/request_info', array('session_id')), array('session_id'))
-    ->addIndex($this->getIdxName('sheep_debug/request_info', array('request_path')), array('request_path'))
-;
+    ->addIndex($this->getIdxName('sheep_debug/request_info', array('request_path')), array('request_path'));
 
 $this->getConnection()->createTable($table);
 
