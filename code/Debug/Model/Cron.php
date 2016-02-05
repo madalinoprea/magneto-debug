a17c2615bb6c6a49d7a99c811078d47e59f2c5d8
Mario O <madalinoprea@gmail.com>
2016-02-05 20:05:24 +0200

    Adds cron job that deletes requests created older than x days #41

---
+++ code/Debug/Model/Cron.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Class Sheep_Debug_Model_Cron
+ *
+ * @category Sheep
+ * @package  Sheep_Subscription
+ * @license  Copyright: Pirate Sheep, 2016, All Rights reserved.
+ * @link     https://piratesheep.com
+ */
+class Sheep_Debug_Model_Cron
+{
+    const DATE_FORMAT = 'Y-m-d H:i:s';
+
+    /**
+     * Cron jobs that deletes expired request info
+     */
+    public function deleteExpiredRequests()
+    {
+        $helper = Mage::helper('sheep_debug');
+        if (!$helper->isEnabled()) {
+            return 'skipped: module is disabled.';
+        }
+
+        if ($helper->getPersistLifetime() == 0) {
+            return 'skipped: lifetime is set to 0';
+        }
+
+        $expirationDate = $this->getExpirationDate(date(self::DATE_FORMAT));
+        $table = $this->getRequestsTable();
+        $deleteSql = "DELETE FROM {$table} WHERE date <= '{$expirationDate}'";
+
+        /** @var Magento_Db_Adapter_Pdo_Mysql $connection */
+        $connection = Mage::getSingleton('core/resource')->getConnection('core_write');
+
+        /** @var Varien_Db_Statement_Pdo_Mysql $result */
+        $result = $connection->query($deleteSql);
+        return "{$result->rowCount()} requests deleted";
+    }
+
+
+    /**
+     * Returns request info table
+     *
+     * @return string
+     */
+    protected function getRequestsTable()
+    {
+        return Mage::getResourceModel('sheep_debug/requestInfo')->getMainTable();
+    }
+
+
+    /**
+     * Removes configured number of days from specified date
+     *
+     * @param string $currentDate
+     * @return bool|string
+     */
+    protected function getExpirationDate($currentDate)
+    {
+        $numberOfDays = Mage::helper('sheep_debug')->getPersistLifetime();
+
+        return date(self::DATE_FORMAT, strtotime("-{$numberOfDays} days {$currentDate}"));
+    }
+
+}
--- code/Debug/Model/Resource/RequestInfo/Collection.php
+++ code/Debug/Model/Resource/RequestInfo/Collection.php
@@ -16,4 +16,18 @@ class Sheep_Debug_Model_Resource_RequestInfo_Collection extends Mage_Core_Model_
         $this->_init('sheep_debug/requestInfo');
     }
 
+    /**
+     * Filters requests that were processed before specified data
+     *
+     * @param string $date Date string using format Y-m-d H
+     * @return Sheep_Debug_Model_Resource_RequestInfo_Collection
+     */
+    public function addEarlierFilter($date)
+    {
+        return $this->addFieldToFilter('date', array(
+            'to'       => $date,
+            'datetime' => true,
+        ));
+    }
+
 }
