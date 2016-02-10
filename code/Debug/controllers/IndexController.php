982de3136bdd25cf1128cee742039afe0ee8fda9
Mario O <madalinoprea@gmail.com>
2016-02-10 09:50:03 +0200

    Adds request profile list page and url for latest request profile #43

--- code/Debug/controllers/IndexController.php
+++ code/Debug/controllers/IndexController.php
@@ -12,13 +12,92 @@ class Sheep_Debug_IndexController extends Sheep_Debug_Controller_Front_Action
 {
 
     /**
+     * @return Sheep_Debug_Model_Resource_RequestInfo_Collection
+     */
+    protected function _getFilteredRequests()
+    {
+        /** @var Sheep_Debug_Model_Resource_RequestInfo_Collection $requests */
+        $requests = Mage::getModel('sheep_debug/requestInfo')->getCollection();
+        $requests->setCurPage(1);
+        $requests->setPageSize(Mage::helper('sheep_debug/filter')->getLimitDefaultValue());
+
+        if ($sessionId = $this->getRequest()->getParam('session_id')) {
+            $requests->addSessionIdFilter($sessionId);
+        }
+
+        if ($ip = $this->getRequest()->getParam('ip')) {
+            $requests->addIpFilter($ip);
+        }
+
+        if ($method = $this->getRequest()->getParam('method')) {
+            $requests->addHttpMethodFilter($method);
+        }
+
+        if ($limit = $this->getRequest()->getParam('limit')) {
+            $requests->setPageSize($limit);
+        }
+
+        if ($path = $this->getRequest()->getParam('path')) {
+            $requests->addRequestPathFilter($path);
+        }
+
+        if ($token = $this->getRequest()->getParam('token')) {
+            $requests->addTokenFilter($token);
+        }
+
+        if ($startDate = $this->getRequest()->getParam('start')) {
+            $requests->addAfterFilter($startDate);
+        }
+
+        if ($endDate = $this->getRequest()->getParam('end')) {
+            $requests->addEarlierFilter($endDate);
+        }
+
+        if ($page = (int)$this->getRequest()->getParam('page')) {
+            $requests->setCurPage($page);
+        }
+
+        $requests->addOrder('id', Varien_Data_Collection_Db::SORT_ORDER_DESC);
+
+        return $requests;
+    }
+
+
+    /**
      * View request profile list
-     * TODO: complete implementation for searchAction
      */
     public function searchAction()
     {
+        /** @var Sheep_Debug_Model_Resource_RequestInfo_Collection $requests */
+        $requests = $this->_getFilteredRequests();
+
         $this->loadLayout('sheep_debug');
+        /** @var Mage_Page_Block_Html $rootBlock */
+        $rootBlock = $this->getLayout()->getBlock('root');
+        $rootBlock->setHeaderTitle($this->__('Request profiles'));
+
+        /** @var Sheep_Debug_Block_View $profileListBlock */
+        $profileListBlock = $this->getLayout()->getBlock('sheep_debug_list');
+        $profileListBlock->setData('results', $requests);
+
         $this->renderLayout();
+    }
+
+
+    /**
+     * View latest request profile
+     */
+    public function latestAction()
+    {
+        /** @var Sheep_Debug_Model_Resource_RequestInfo_Collection $profileRequests */
+        $profileRequests = Mage::getModel('sheep_debug/requestInfo')->getCollection();
+        $profileRequests->addOrder('id', Varien_Data_Collection_Db::SORT_ORDER_DESC);
+        $profileRequests->setCurPage(1)->setPageSize(1);
+
+        /** @var Sheep_Debug_Model_RequestInfo $lastProfileRequest */
+        $lastProfileRequest = $profileRequests->getFirstItem();
+
+        $this->_forward('view', null, null, array('token' => $lastProfileRequest->getToken()));
     }
 
 
@@ -27,12 +106,13 @@ class Sheep_Debug_IndexController extends Sheep_Debug_Controller_Front_Action
      */
     public function viewAction()
     {
-        $token = (string) $this->getRequest()->getParam('token');
+        $token = (string)$this->getRequest()->getParam('token');
         if (!$token) {
             $this->getResponse()->setHttpResponseCode(400);
             return $this->_getRefererUrl();
         }
 
+        /** @var Sheep_Debug_Model_RequestInfo $requestInfo */
         $requestInfo = Mage::getModel('sheep_debug/requestInfo')->load($token, 'token');
         if (!$requestInfo->getId()) {
             $this->getResponse()->setHttpResponseCode(404);
@@ -52,8 +132,10 @@ class Sheep_Debug_IndexController extends Sheep_Debug_Controller_Front_Action
         // Add section block to content area
         $this->loadLayout();
         $layout = $this->getLayout();
-        $sectionBlock = $layout->createBlock('sheep_debug/view', $blockName , array('template' => $blockTemplate));
+        $sectionBlock = $layout->createBlock('sheep_debug/view', $blockName, array('template' => $blockTemplate));
         $layout->getBlock('sheep_debug_content')->insert($sectionBlock);
+
+        $layout->getBlock('root')->setHeaderTitle($this->__('Profile for request %s (%s)', $requestInfo->getRequestPath(), $requestInfo->getToken()));
 
         $this->renderLayout();
     }
