3fc285e44960c899082aa5e24a0a9fef2ca064a5
Mario O <madalinoprea@gmail.com>
2016-02-11 22:21:26 +0200

    Adds performance section on request profile page (varien profile timers)

---
+++ code/Debug/Helper/Performance.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * Class Sheep_Debug_Helper_Performance
+ *
+ * @category Sheep
+ * @package  Sheep_Subscription
+ * @license  Copyright: Pirate Sheep, 2016, All Rights reserved.
+ * @link     https://piratesheep.com
+ */
+class Sheep_Debug_Helper_Performance extends Mage_Core_Helper_Abstract
+{
+    const SECTION = 'section';
+
+    const CORE_CATEGORY = 'Core';
+    const CONFIG_CATEGORY = 'Config';
+    const EAV_CATEGORY = 'doctrine';
+    const DB_CATEGORY = 'doctrine';
+    const LAYOUT_CATEGORY = 'template';
+    const EVENT_CATEGORY = 'event_listener';
+
+
+    /**
+     * Determines category based on timer name
+     *
+     * @param $timerName
+     * @return string
+     */
+    public function getCategory($timerName)
+    {
+        $category = self::CORE_CATEGORY;
+
+        if (strpos($timerName, 'mage::dispatch') === 0 || strpos($timerName, 'column.phtml') > 0) {
+            $category = self::SECTION;
+        } else if (strpos($timerName, 'Model_Resource') > 0) {
+            $category = self::DB_CATEGORY;
+        } else if (strpos($timerName, 'EAV') === 0 || strpos($timerName, '_LOAD_ATTRIBUTE_') === 0 || strpos($timerName, '__EAV_') === 0) {
+            $category = self::EAV_CATEGORY;
+        } else if (strpos($timerName, 'CORE::create_object_of') === 0) {
+            $category = self::CORE_CATEGORY;
+        } else if (strpos($timerName, 'OBSERVER') === 0 || strpos($timerName, 'DISPATCH EVENT') === 0) {
+            $category = self::EVENT_CATEGORY;
+        } else if (strpos($timerName, 'BLOCK') === 0) {
+            $category = self::LAYOUT_CATEGORY;
+        } else if (strpos($timerName, 'init_config') === 0) {
+            $category = self::CONFIG_CATEGORY;
+        } else if (strpos($timerName, 'layout/') === 0 || strpos($timerName, 'layout_') > 0) {
+            $category = self::LAYOUT_CATEGORY;
+        } else if (strpos($timerName, 'Mage_Core_Model_Design') === 0) {
+            $category = self::LAYOUT_CATEGORY;
+        } else if (strpos($timerName, '.phtml') > 0) {
+            $category = self::LAYOUT_CATEGORY;
+        }
+
+        return $category;
+    }
+
+
+    /**
+     * Converts timers registered by Varien Profile into structure understood by Symfony
+     *
+     * @param Sheep_Debug_Model_RequestInfo $request
+     * @return array
+     */
+    public function convertTimers(Sheep_Debug_Model_RequestInfo $request)
+    {
+        if (!$request->getTimers()) {
+            return array();
+        }
+
+        $requestData = array();
+        $requestData['id'] = $request->getToken();
+        $requestData['left'] = 0;
+        $requestData['events'] = array();
+
+        $currentTime = 0;
+        /**  timer is an array defined by @see Varien_Profiler */
+        foreach ($request->getTimers() as $name => $timer) {
+
+            if (!$timer['sum']) {
+                continue;
+            }
+
+            // convert seconds into microseconds
+            $timer['sum'] *= 1000;
+
+            $category = $this->getCategory($name);
+
+            // we don't have start time, end time or multiple periods
+            // so built an estimation of how this event might look assuming that we have only one
+            // occurrence (period) and that it followed immediately after last event
+            $requestData['events'][] = array(
+                'name'      => $name,
+                'category'  => $category,
+                'origin'    => 0,
+                'starttime' => $currentTime,
+                'endtime'   => $currentTime + $timer['sum'],
+                'duration'  => $timer['sum'],
+                'memory'    => round($timer['realmem'] / pow(2014, 2), 2),
+                'periods'   => array(
+                    array('start' => $currentTime, 'end' => $currentTime + $timer['sum'])
+                )
+            );
+
+            // we assume certain events contain the other events (most of the time it makes sense)
+            if ($category != self::SECTION) {
+                $currentTime += $timer['sum'];
+            }
+        }
+
+        $data = array();
+        $data['max'] = $currentTime;
+        $data['requests'] = array($requestData);
+
+        return $data;
+    }
+
+}
--- skin/js/profile.js
+++ skin/js/profile.js
@@ -447,3 +447,316 @@ function sortTable(header, column, targetId) {
     }
 }
 
+/**
+ * In-memory key-value cache manager
+ */
+var cache = new function () {
+    "use strict";
+    var dict = {};
+
+    this.get = function (key) {
+        return dict.hasOwnProperty(key)
+            ? dict[key]
+            : null;
+    };
+
+    this.set = function (key, value) {
+        dict[key] = value;
+
+        return value;
+    };
+};
+
+/**
+ * Query an element with a CSS selector.
+ *
+ * @param string selector a CSS-selector-compatible query string.
+ *
+ * @return DOMElement|null
+ */
+function query(selector) {
+    "use strict";
+    var key = 'SELECTOR: ' + selector;
+
+    return cache.get(key) || cache.set(key, document.querySelector(selector));
+}
+
+/**
+ * Canvas Manager
+ */
+function CanvasManager(requests, maxRequestTime) {
+    "use strict";
+
+    var _drawingColors = {
+            "default": "#999",
+            "section": "#444",
+            "event_listener": "#00B8F5",
+            "event_listener_loading": "#00B8F5",
+            "template": "#66CC00",
+            "doctrine": "#FF6633",
+            "propel": "#FF6633"
+        },
+        _storagePrefix = 'timeline/',
+        _threshold = 1,
+        _requests = requests,
+        _maxRequestTime = maxRequestTime;
+
+    /**
+     * Check whether this event is a child event.
+     *
+     * @return true if it is.
+     */
+    function isChildEvent(event) {
+        return '__section__.child' === event.name;
+    }
+
+    /**
+     * Check whether this event is categorized in 'section'.
+     *
+     * @return true if it is.
+     */
+    function isSectionEvent(event) {
+        return 'section' === event.category;
+    }
+
+    /**
+     * Get the width of the container.
+     */
+    function getContainerWidth() {
+        return query('#collector-content h2').clientWidth;
+    }
+
+    /**
+     * Draw one canvas.
+     *
+     * @param request   the request object
+     * @param max       <subjected for removal>
+     * @param threshold the threshold (lower bound) of the length of the timeline (in milliseconds).
+     * @param width     the width of the canvas.
+     */
+    this.drawOne = function (request, max, threshold, width) {
+        "use strict";
+        var text,
+            ms,
+            xc,
+            drawableEvents,
+            mainEvents,
+            elementId = 'timeline_' + request.id,
+            canvasHeight = 0,
+            gapPerEvent = 38,
+            colors = _drawingColors,
+            space = 10.5,
+            ratio = (width - space * 2) / max,
+            h = space,
+            x = request.left * ratio + space, // position
+            canvas = cache.get(elementId) || cache.set(elementId, document.getElementById(elementId)),
+            ctx = canvas.getContext("2d"),
+            scaleRatio,
+            devicePixelRatio;
+
+        // Filter events whose total time is below the threshold.
+        drawableEvents = request.events.filter(function (event) {
+            return event.duration >= threshold;
+        });
+
+        canvasHeight += gapPerEvent * drawableEvents.length;
+
+        // For retina displays so text and boxes will be crisp
+        devicePixelRatio = window.devicePixelRatio == "undefined" ? 1 : window.devicePixelRatio;
+        scaleRatio = devicePixelRatio / 1;
+
+        canvas.width = width * scaleRatio;
+        canvas.height = canvasHeight * scaleRatio;
+
+        canvas.style.width = width + 'px';
+        canvas.style.height = canvasHeight + 'px';
+
+        ctx.scale(scaleRatio, scaleRatio);
+
+        ctx.textBaseline = "middle";
+        ctx.lineWidth = 0;
+
+        // For each event, draw a line.
+        ctx.strokeStyle = "#CCC";
+
+        drawableEvents.forEach(function (event) {
+            event.periods.forEach(function (period) {
+                var timelineHeadPosition = x + period.start * ratio;
+
+                if (isChildEvent(event)) {
+                    /* create a striped background dynamically */
+                    var img = new Image();
+                    img.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKBAMAAAB/HNKOAAAAIVBMVEX////w8PDd7h7d7h7d7h7d7h7w8PDw8PDw8PDw8PDw8PAOi84XAAAAKUlEQVQImWNI71zAwMBQMYuBgY0BxExnADErGEDMTgYQE8hnAKtCZwIAlcMNSR9a1OEAAAAASUVORK5CYII=';
+                    var pattern = ctx.createPattern(img, 'repeat');
+
+                    ctx.fillStyle = pattern;
+                    ctx.fillRect(timelineHeadPosition, 0, (period.end - period.start) * ratio, canvasHeight);
+                } else if (isSectionEvent(event)) {
+                    var timelineTailPosition = x + period.end * ratio;
+
+                    ctx.beginPath();
+                    ctx.moveTo(timelineHeadPosition, 0);
+                    ctx.lineTo(timelineHeadPosition, canvasHeight);
+                    ctx.moveTo(timelineTailPosition, 0);
+                    ctx.lineTo(timelineTailPosition, canvasHeight);
+                    ctx.fill();
+                    ctx.closePath();
+                    ctx.stroke();
+                }
+            });
+        });
+
+        // Filter for main events.
+        mainEvents = drawableEvents.filter(function (event) {
+            return !isChildEvent(event)
+        });
+
+        // For each main event, draw the visual presentation of timelines.
+        mainEvents.forEach(function (event) {
+
+            h += 8;
+
+            // For each sub event, ...
+            event.periods.forEach(function (period) {
+                // Set the drawing style.
+                ctx.fillStyle = colors['default'];
+                ctx.strokeStyle = colors['default'];
+
+                if (colors[event.name]) {
+                    ctx.fillStyle = colors[event.name];
+                    ctx.strokeStyle = colors[event.name];
+                } else if (colors[event.category]) {
+                    ctx.fillStyle = colors[event.category];
+                    ctx.strokeStyle = colors[event.category];
+                }
+
+                // Draw the timeline
+                var timelineHeadPosition = x + period.start * ratio;
+
+                if (!isSectionEvent(event)) {
+                    ctx.fillRect(timelineHeadPosition, h + 3, 2, 8);
+                    ctx.fillRect(timelineHeadPosition, h, (period.end - period.start) * ratio || 2, 6);
+                } else {
+                    var timelineTailPosition = x + period.end * ratio;
+
+                    ctx.beginPath();
+                    ctx.moveTo(timelineHeadPosition, h);
+                    ctx.lineTo(timelineHeadPosition, h + 11);
+                    ctx.lineTo(timelineHeadPosition + 8, h);
+                    ctx.lineTo(timelineHeadPosition, h);
+                    ctx.fill();
+                    ctx.closePath();
+                    ctx.stroke();
+
+                    ctx.beginPath();
+                    ctx.moveTo(timelineTailPosition, h);
+                    ctx.lineTo(timelineTailPosition, h + 11);
+                    ctx.lineTo(timelineTailPosition - 8, h);
+                    ctx.lineTo(timelineTailPosition, h);
+                    ctx.fill();
+                    ctx.closePath();
+                    ctx.stroke();
+
+                    ctx.beginPath();
+                    ctx.moveTo(timelineHeadPosition, h);
+                    ctx.lineTo(timelineTailPosition, h);
+                    ctx.lineTo(timelineTailPosition, h + 2);
+                    ctx.lineTo(timelineHeadPosition, h + 2);
+                    ctx.lineTo(timelineHeadPosition, h);
+                    ctx.fill();
+                    ctx.closePath();
+                    ctx.stroke();
+                }
+            });
+
+            h += 30;
+
+            ctx.beginPath();
+            ctx.strokeStyle = "#E0E0E0";
+            ctx.moveTo(0, h - 10);
+            ctx.lineTo(width, h - 10);
+            ctx.closePath();
+            ctx.stroke();
+        });
+
+        h = space;
+
+        // For each event, draw the label.
+        mainEvents.forEach(function (event) {
+
+            ctx.fillStyle = "#444";
+            ctx.font = "12px sans-serif";
+            text = event.name;
+            ms = "  " + (event.duration < 1 ? event.duration : parseInt(event.duration, 10)) + " ms / " + event.memory + " MB";
+            if (x + event.starttime * ratio + ctx.measureText(text + ms).width > width) {
+                ctx.textAlign = "end";
+                ctx.font = "10px sans-serif";
+                ctx.fillStyle = "#777";
+                xc = x + event.endtime * ratio - 1;
+                ctx.fillText(ms, xc, h);
+
+                xc -= ctx.measureText(ms).width;
+                ctx.font = "12px sans-serif";
+                ctx.fillStyle = "#222";
+                ctx.fillText(text, xc, h);
+            } else {
+                ctx.textAlign = "start";
+                ctx.font = "13px sans-serif";
+                ctx.fillStyle = "#222";
+                xc = x + event.starttime * ratio + 1;
+                ctx.fillText(text, xc, h);
+
+                xc += ctx.measureText(text).width;
+                ctx.font = "11px sans-serif";
+                ctx.fillStyle = "#777";
+                ctx.fillText(ms, xc, h);
+            }
+
+            h += gapPerEvent;
+        });
+    };
+
+    this.drawAll = function (width, threshold) {
+        "use strict";
+
+        width = width || getContainerWidth();
+        threshold = threshold || this.getThreshold();
+
+        var self = this;
+
+        _requests.forEach(function (request) {
+            self.drawOne(request, _maxRequestTime, threshold, width);
+        });
+    };
+
+    this.getThreshold = function () {
+        var threshold = Sfjs.getPreference(_storagePrefix + 'threshold');
+
+        if (null === threshold) {
+            return _threshold;
+        }
+
+        _threshold = parseInt(threshold);
+
+        return _threshold;
+    };
+
+    this.setThreshold = function (threshold) {
+        _threshold = threshold;
+
+        Sfjs.setPreference(_storagePrefix + 'threshold', threshold);
+
+        return this;
+    };
+}
+
+function canvasAutoUpdateOnResizeAndSubmit(e) {
+    e.preventDefault();
+    canvasManager.drawAll();
+}
+
+function canvasAutoUpdateOnThresholdChange(e) {
+    canvasManager
+        .setThreshold(query('input[name="threshold"]').value)
+        .drawAll();
+}
