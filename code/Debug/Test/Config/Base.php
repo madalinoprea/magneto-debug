3c1fbe7670daba5dcca1d605f4b0ceaedc3b0947
Mario O <madalinoprea@gmail.com>
2016-02-13 15:48:26 +0200

    Adds unit test for our configuration #44

--- code/Debug/Test/Config/Base.php
+++ code/Debug/Test/Config/Base.php
@@ -14,6 +14,18 @@ class Sheep_Debug_Test_Config_Base extends EcomDev_PHPUnit_Test_Case_Config
     public function testModelAliases()
     {
         $this->assertModelAlias('sheep_debug/resourceInfo', 'Sheep_Debug_Model_ResourceInfo');
+        $this->assertModelAlias('core/email', 'Sheep_Debug_Model_Core_Email');
+        $this->assertModelAlias('core/email_template', 'Sheep_Debug_Model_Core_Email_Template');
+    }
+
+    public function testSetup()
+    {
+        $this->assertSetupResourceDefined('Sheep_Debug', 'sheep_debug_setup');
+    }
+
+    public function testBlocks()
+    {
+        $this->assertBlockAlias('sheep_debug/abstract', 'Sheep_Debug_Block_Abstract');
     }
 
     public function testHelperAlias()
@@ -21,13 +33,37 @@ class Sheep_Debug_Test_Config_Base extends EcomDev_PHPUnit_Test_Case_Config
         $this->assertHelperAlias('sheep_debug', 'Sheep_Debug_Helper_Data');
     }
 
-    public function testSetup()
-    {
-        $this->assertSetupResourceDefined('Sheep_Debug', 'sheep_debug_setup');
-    }
-    
     public function testObservers()
     {
-        // TODO: implement observer
+        $this->assertEventObserverDefined('global', 'controller_front_init_before', 'sheep_debug/observer', 'onControllerFrontInitBefore');
+        $this->assertEventObserverDefined('global', 'controller_action_layout_generate_blocks_after', 'sheep_debug/observer', 'onLayoutGenerate');
+        $this->assertEventObserverDefined('global', 'core_block_abstract_to_html_before', 'sheep_debug/observer', 'onBlockToHtml');
+        $this->assertEventObserverDefined('global', 'core_block_abstract_to_html_after', 'sheep_debug/observer', 'onBlockToHtmlAfter');
+        $this->assertEventObserverDefined('global', 'controller_action_postdispatch', 'sheep_debug/observer', 'onActionPostDispatch');
+        $this->assertEventObserverDefined('global', 'core_collection_abstract_load_before', 'sheep_debug/observer', 'onCollectionLoad');
+        $this->assertEventObserverDefined('global', 'eav_collection_abstract_load_before', 'sheep_debug/observer', 'onCollectionLoad');
+        $this->assertEventObserverDefined('global', 'model_load_after', 'sheep_debug/observer', 'onModelLoad');
+        $this->assertEventObserverDefined('global', 'controller_action_predispatch', 'sheep_debug/observer', 'onActionPreDispatch');
+        $this->assertEventObserverDefined('global', 'controller_front_send_response_after', 'sheep_debug/observer', 'onControllerFrontSendResponseAfter');
     }
+
+    public function testFrontend()
+    {
+        $this->assertRouteIn('sheep_debug');
+        $this->assertLayoutFileDefined('frontend', 'sheep_debug.xml');
+    }
+
+    public function testAdmin()
+    {
+        $this->assertLayoutFileDefined('adminhtml', 'sheep_debug.xml');
+    }
+
+    public function testDefaultConfigs()
+    {
+        $this->assertDefaultConfigValue('sheep_debug/options/enable', 1);
+        $this->assertDefaultConfigValue('sheep_debug/options/persist', 1);
+        $this->assertDefaultConfigValue('sheep_debug/options/persist_expiration', 1);
+        $this->assertDefaultConfigValue('sheep_debug/options/force_varien_profile', 1);
+    }
+
 }
