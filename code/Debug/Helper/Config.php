bc2f8de2d0013d0655adc61a800d952a1eb65065
Mario O <madalinoprea@gmail.com>
2016-02-13 20:41:27 +0200

    Adds php doc for sheep_debug/config helper

--- code/Debug/Helper/Config.php
+++ code/Debug/Helper/Config.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Class Sheep_Debug_Helper_Config
+ *
+ * @category Sheep
+ * @package  Sheep_Debug
+ * @license  Copyright: Pirate Sheep, 2016, All Rights reserved.
+ * @link     https://piratesheep.com
+ */
 class Sheep_Debug_Helper_Config extends Mage_Core_Helper_Abstract
 {
 
