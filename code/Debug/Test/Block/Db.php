3625ce4d1b5ffea862a5ac8c46217195bb82b254
Mario O <madalinoprea@gmail.com>
2016-02-13 21:02:51 +0200

    Adds unit tests for Sheep_Debug_Block_Db #44

---
+++ code/Debug/Test/Block/Db.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Class Sheep_Debug_Test_Block_Db
+ *
+ * @category Sheep
+ * @package  Sheep_Debug
+ * @license  Copyright: Pirate Sheep, 2016, All Rights reserved.
+ * @link     https://piratesheep.com
+ *
+ * @covers Sheep_Debug_Block_Db
+ * @codeCoverageIgnore
+ */
+class Sheep_Debug_Test_Block_Db extends EcomDev_PHPUnit_Test_Case
+{
+
+    public function testIsSqlProfilerEnabled()
+    {
+        $profiler = $this->getMock('Zend_Db_Profiler', array('getEnabled'));
+        $profiler->expects($this->once())->method('getEnabled')->willReturn(true);
+
+        $helper = $this->getHelperMock('sheep_debug', array('getSqlProfiler'));
+        $helper->expects($this->once())->method('getSqlProfiler')->willReturn($profiler);
+        $this->replaceByMock('helper', 'sheep_debug', $helper);
+
+        $block = $this->getBlockMock('sheep_debug/db', array('toHtml'));
+        $this->assertTrue($block->isSqlProfilerEnabled());
+    }
+
+}
